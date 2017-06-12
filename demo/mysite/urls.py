abf8915b2cdf9b758fd96aa1738e7aeab369a33f
zhaomingrui <zhaomingrui@chunyu.me>
2017-06-12 15:59:59 +0800

    1.added login page and logic  2.added a simple index page

--- demo/mysite/urls.py
+++ demo/mysite/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('mysite.views',
                        url(r'^$', 'index'),
+                       url(r'^login$', 'login'),
+                       url(r'^login_check$', 'login_check')
                        )
--- demo/mysite/views.py
+++ demo/mysite/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, redirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate
+from django.template import RequestContext
+
 from models import Article, Reply
 
-# Create your views here.
 
+
+# index page
 def index(request):
     latest_article_list = Article.objects.all().order_by('-pub_date')[:10]
     return render_to_response('mysite/index.html', {'latest_article_list': latest_article_list})
+
+# login page
+def login(request):
+    return render_to_response('mysite/login.html', {}, context_instance=RequestContext(request))
+
+# check username and password from db
+def login_check(request):
+    if request.method == 'POST':
+        uname = request.POST['username']
+        pw = request.POST['password']
+        user = authenticate(username=uname, password=pw)
+        if user is not None:
+            return redirect(reverse('mysite.views.index'))
+        else:
+            return redirect(reverse('mysite.views.login'))
+    return redirect(reverse('mysite.views.login'))
